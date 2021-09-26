db9046e38e002b43c15c5bc4feeb8601726435ea
Rameshwari-Joshi-2001 <77911205+Rameshwari-Joshi-2001@users.noreply.github.com>
2021-09-26 17:14:19 +0530

    Update Current_Acc.java

--- Current_Acc.java
+++ Current_Acc.java
@@ -1,82 +1,54 @@
 package logic;
+//package abstractBank;
 
+public class CurrentAccount extends Account {
+	private static double overdraft = 5000;
 
-public class Current_Acc  extends Account{
-	//Attributes
-	private int trans_count=0;
-	private static final int  Min_trans=3;
-	private static final double trans_Fee= 40.0;
-
-	public Current_Acc(String AccNo, String AccOwN) {
-		super(AccNo, AccOwN);
-		trans_count=0;
-	}	
-	//method Override
-	public boolean withdraw(int Amount)
-	{
-		boolean ret; 
-		if(super.withdraw(Amount))   //callng a method of super class
+	public CurrentAccount(double balance,String name) {
+		super(balance, name);
+	}
+	public CurrentAccount(int accNo) {
+		super(accNo);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public boolean withdraw(double amt) {
+		
+		double checkbalance  = this.getBalance()- amt;
+		if(checkbalance < overdraft)
 		{
-			trans_count++;
-			ret=true;
+			System.out.println("A letter is sent to the acc holder that overdraft limit" +
+					" is crossed");
+			return false;
 		}
 		else
 		{
-			System.out.println("Not have sufficient Amount Re-enter the Amount!");
-			ret = false;
+			this.setBalance(checkbalance);
+			return true;
+			
 		}
-		return ret;
+		
 	}
-	public boolean deposite(int Amount)
-	{
-		boolean ret;
-		if(super.deposite(Amount))
+
+	@Override
+	public void deposit(double amt) {
+		double checkBalance = this.getBalance() + amt;
+		this.setBalance(checkBalance);
+
+	}
+
+	@Override
+	public void transfer(double amt, Account toAccount) {
+		
+		if(this.withdraw(amt) == true)
 		{
-			trans_count++;
-			ret=false;
+			toAccount.deposit(amt);	
 		}
 		else
 		{
-			System.out.println("Please enter valid Amount!");
-			ret=false;
+			System.out.println("Transfer is not possible");
 		}
-		return ret;
+		
+		
 	}
-	public void fee_deduction()
-	{
-		if(trans_count > Min_trans)
-		{
-			super.withdraw((trans_count-Min_trans)*40.0);
-			System.out.println("Fee deducted for your transactions!");
-		}
-		else 
-		{			
-				System.out.println("No fee deducted! Happy transactions!");
-		}
-	}
-	public static void main(String[] args) {
-		Current_Acc Cur_Acc_Holder1 = new Current_Acc("11111",
-	   			"Ram Raju Rakhi");
-
-		Cur_Acc_Holder1.deposite(5000);
-		Cur_Acc_Holder1.withdraw(2000);
-		Cur_Acc_Holder1.deposite(7000);
-	   	// No deduction fee (only 3 transactions)
-		Cur_Acc_Holder1.fee_deduction();
-	   	System.out.println("transactions less than Min_trans: " + Cur_Acc_Holder1.getAccBalance());
-	   	
-	   	Cur_Acc_Holder1.deposite(4000);
-	   	// Deduction of fee (4 transactions)
-	   	Cur_Acc_Holder1.fee_deduction();
-	   	System.out.println("transactions grater than Min_trans " + Cur_Acc_Holder1.getAccBalance());
-	   }
 }
-
-
-
-
-
-
-
-
-
